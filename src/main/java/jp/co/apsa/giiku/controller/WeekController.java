5e80455997ed3f031d906c34c6e376d2aa4733e5
kterai <k_terai@apsa.co.jp>
2025-08-10 13:12:33 +0900

    レイアウト調整

--- src/main/java/jp/co/apsa/giiku/controller/WeekController.java
+++ src/main/java/jp/co/apsa/giiku/controller/WeekController.java
@@ -1,5 +1,6 @@
 package jp.co.apsa.giiku.controller;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,11 +29,13 @@ public class WeekController  extends AbstractController{
     @GetMapping(path = {"/{page}","/{page}.html"})
     public String week(@PathVariable String page, Model model) {
         setTitle(model, "週別詳細");
-        String title = "Week " + page.replace("week", "");
+        String title = "第" + page.replace("week", "") + "週目";
+        int monthNumber = ((NumberUtils.toInt(page.replace("week", ""), 1)) / 4 + 1);
+        String monthTitle = "第" + monthNumber + "ヶ月目";
         model.addAttribute("pageTitle", title);
         model.addAttribute("breadcrumb", List.of(
-                Map.of("label", "Home", "href", "/"),
-                Map.of("label", "Week", "href", "/week"),
+                Map.of("label", "ホーム", "href", "/"),
+                Map.of("label", monthTitle, "href", "/month" + monthNumber),
                 Map.of("label", title)
         ));
         return "week/" + page;
