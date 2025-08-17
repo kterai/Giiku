8a05d3031c86c797bf89b84025ae17470ecf0a99
kterai <k_terai@apsa.co.jp>
2025-08-17 20:03:29 +0900

    Rename SecurityConfig for tests

--- src/test/java/jp/co/apsa/giiku/infrastructure/security/TestSecurityConfig.java
+++ src/test/java/jp/co/apsa/giiku/infrastructure/security/TestSecurityConfig.java
@@ -1,22 +1,22 @@
 package jp.co.apsa.giiku.infrastructure.security;
 
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
- * セキュリティ設定クラス
- * 
+ * テスト用セキュリティ設定クラス
+ *
  * @author 株式会社アプサ
  * @version 1.0
  * @since 2025
  */
-@Configuration
+@TestConfiguration
 @EnableWebSecurity
-public class SecurityConfig {
+public class TestSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
