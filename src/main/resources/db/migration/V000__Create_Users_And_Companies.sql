d3bb0ec54dd6d6054a870d7662466fa022db1c46
kterai <k_terai@apsa.co.jp>
2025-08-22 05:41:25 +0900

    微修正

--- src/main/resources/db/migration/V000__Create_Users_And_Companies.sql
+++ src/main/resources/db/migration/V000__Create_Users_And_Companies.sql
@@ -104,7 +104,7 @@ VALUES
     (1, 'admin', '$2b$12$wSzqgNX0.nsNqrAvyGyJbudYp8wkjOdv52kZTSSFz0Sj2gCA7v4I2', 'admin@apsa.co.jp', '管理者 太郎', 1, 'ADMIN', 1, '1980-01-01', true, 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
     (2, 'instructor', '$2b$12$wSzqgNX0.nsNqrAvyGyJbudYp8wkjOdv52kZTSSFz0Sj2gCA7v4I2', 'instructor@apsa.co.jp', '講師 花子', 1, 'INSTRUCTOR', 2, '1985-05-05', true, 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
     (3, 'trainee_apsa', '$2b$12$wSzqgNX0.nsNqrAvyGyJbudYp8wkjOdv52kZTSSFz0Sj2gCA7v4I2', 'trainee1@apsa.co.jp', '受講者 一郎', 1, 'TRAINEE', 1, '1995-03-03', true, 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    (4, 'trainee_ext', '$2b$12$wSzqgNX0.nsNqrAvyGyJbudYp8wkjOdv52kZTSSFz0Sj2gCA7v4I2', 'trainee2@trainee.co.jp', '受講者 二郎', 2, 'TRAINEE', 3, '1996-04-04', true, 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
+    (4, 'trainee_ext', '$2b$12$wSzqgNX0.nsNqrAvyGyJbudYp8wkjOdv52kZTSSFz0Sj2gCA7v4I2', 'trainee2@trainee.co.jp', '受講者 二郎', 2, 'TRAINEE', 3, '1996-04-04', true, 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP);
 
 
 -- ========================================
